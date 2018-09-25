fc207d98e302984bd320c204f844d22ca8e20151
Raf Ceuls <rcl@foreach.be>
2018-09-25 20:27:58 +0200

    Added presentation for basic C# things

---
+++ exercises/Basic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Basic
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      String name = "xyz";
+      var nameAsVar = "abc";
+      Console.WriteLine($"{name} - {nameAsVar}");
+
+    }
+  }
+}
